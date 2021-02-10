4f456a05db8a4706eb2dd9449ede9524742d5374
Andres <castroandresrm@gmail.com>
2021-02-10 15:21:36 -0400

    Agregamos articulos a las compras

--- pagar.php
+++ pagar.php
@@ -11,11 +11,19 @@ require 'config.php';
 $producto = htmlspecialchars($_POST['producto']);
 $precio = htmlspecialchars($_POST['precio']);
 $precio = (int)$precio;
-var_dump($precio);
+//var_dump($precio);
 $envio = 0;
 $total = $precio + $envio;
 
 $compra = new Payer();
 $compra->setPaymentMethod('paypal');
-echo $compra->getPaymentMethod();//obter el metodo de pago
+//echo $compra->getPaymentMethod();//obter el metodo de pago
 $articulo = new Item();
+$articulo->setName($producto)
+         ->setCurrency('USD')
+         ->setQuantity(1)
+         ->setPrice($precio);
+
+echo $articulo->getQuantity();
+echo $articulo->getName();
+echo $articulo->getPrice();
