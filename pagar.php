736677d4b048f080aada92e8a9ebf7861b6836ba
Andres <castroandresrm@gmail.com>
2021-02-09 20:42:45 -0400

    Llamamos a las clases y metodos para recibir pagos

--- pagar.php
+++ pagar.php
@@ -4,6 +4,9 @@ if(!isset($_POST['producto'], $_POST['precio'])){
     exit('Hubo un error');
 }
 
+use PayPal\Api\Payer;
+use PayPal\Api\Item;
+
 require 'config.php';
 $producto = htmlspecialchars($_POST['producto']);
 $precio = htmlspecialchars($_POST['precio']);
@@ -11,3 +14,8 @@ $precio = (int)$precio;
 var_dump($precio);
 $envio = 0;
 $total = $precio + $envio;
+
+$compra = new Payer();
+$compra->setPaymentMethod('paypal');
+echo $compra->getPaymentMethod();//obter el metodo de pago
+$articulo = new Item();
