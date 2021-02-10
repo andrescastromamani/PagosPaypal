055c0059819001e1a7c16edce41379203a2dbe6f
Andres <castroandresrm@gmail.com>
2021-02-09 20:13:29 -0400

    Agregamos el SDK de PayPal y creamos el archivo Pagar.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+ require 'paypal/autoload.php';
+
+ $apiContext = new \PayPal\Rest\ApiContext(
+    new \PayPal\Auth\OAuthTokenCredential(
+        'AZLeOdFl-Kx89QDQzv_NYycoq4DWRvZWEtWS0WaZFKtgFjpmxT6Swq3ETFYAayXSC-9E1BLoX8BAvnOJ',//cliente ID
+        'EClDDXCngMunRubsXsyQQYIuTEqzL7vK4mjIlBw_oyNNKdZltpI_Ns7hKOVXqc_1abq5hVtHcvkdDsXH'//Secret
+    )
+ );
+
+ var_dump($apiContext);
